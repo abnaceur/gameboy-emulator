7c0bbaf6ca25932a04a1c2e002a0b1a0381bd6b5
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2020-03-05 00:09:40 +0100

    ADMIN : - Table design added.

---
+++ precache-manifest.d9aed18bec741cd13d31a6b0a40c847a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb532e08070ef9d898047597cc4cd087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6d786d2c76780c77fb5",
+    "url": "/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "d229f752bff4d63359a2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f6d786d2c76780c77fb5",
+    "url": "/static/js/2.f29dc3cb.chunk.js"
+  },
+  {
+    "revision": "d229f752bff4d63359a2",
+    "url": "/static/js/main.2e54c89d.chunk.js"
+  },
+  {
+    "revision": "0d2e79cb4bb297c69d4a",
+    "url": "/static/js/runtime-main.28f875cd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
